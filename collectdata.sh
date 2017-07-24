c6d28a5ba954969c079a08ed5e7d80872c831586
lydiaxnguyen <lydiaxnguyen@gmail.com>
2017-07-24 12:52:22 -0400

    created assignment 4 stuff

---
+++ collectdata.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Survey questions
+
+# Ask question 1: what is their first and last name
+echo "What is your first and last name?"
+read name
+
+# Ask question 2: what is your field of study/major
+echo "What is your field of study/major?"
+read major
+
+# Ask question 3: what year were you born?
+echo "What year were you born?"
+read year
+
+# Ask question 4: Where have you spent most of your life?
+echo "What language do you wish you knew?"
+read dreams
+
+# Ask question 5: What do you think is the most pressing issue of our time?
+echo "What do you think is the most pressing issue of our time?"
+read worldissues
+
+#save the time
+echo `date --iso-8601`
+read date
+
+#create unique identifier
+
+# write data to CSV file
+echo "$name, $major, $year, $dreams, $worldissues, $date" >> surveyanswers.csv
+
